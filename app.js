b57a2686f2ae37ef620ed9fe266d7d60e71e92d0
Jay Huang <jhuang71@hawk.iit.edu>
2019-05-29 20:04:37 -0500

    modify connection to MongodbAtlas

--- app.js
+++ app.js
@@ -17,7 +17,17 @@ var commentRoutes = require('./routes/comments'),
 // seedDB();
 
 // mongoose.connect('mongodb://localhost/yelp_camp');
-mongoose.connect('mongodb+srv://JayH:1022JIAWEIhuang!@cluster0-p9mck.mongodb.net/test?retryWrites=true&w=majority');
+mongoose
+	.connect('mongodb+srv://JayH:1022JIAWEIhuang!@cluster0-p9mck.mongodb.net/test?retryWrites=true&w=majority', {
+		useNewUrlParser: true,
+		useCreateIndex: true
+	})
+	.then(() => {
+		console.log('Connected to DB!');
+	})
+	.catch((err) => {
+		console.log('ERROR: ', err.message);
+	});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
